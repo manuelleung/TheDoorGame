13017bcf9a36decb3ad267840261d98ec6275122
Manuel <manuelleung93@gmail.com>
2016-01-21 00:17:25 -0500

    Project template

---
+++ desktop/src/org/thedoorgame/doorgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.thedoorgame.doorgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.thedoorgame.doorgame.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
