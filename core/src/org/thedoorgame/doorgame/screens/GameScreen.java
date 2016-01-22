8ee6d9a3707352f45e0ec6f246c26d4aa92edcd6
iLenny <Leibniz.berihuete@gmail.com>
2016-01-22 11:34:50 -0500

    Leibniz added graphics images to assets, and modified GameScreen with Gridlines for design purposes

--- core/src/org/thedoorgame/doorgame/screens/GameScreen.java
+++ core/src/org/thedoorgame/doorgame/screens/GameScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import org.thedoorgame.doorgame.DoorGame;
 
@@ -20,6 +21,11 @@ public class GameScreen implements Screen {
 
     private int width, height;
 
+// -------------- ADDED BY LEIBNIZ ---------------//
+    private static final int GRID_CELL = 32;      //    This is for grid-display and
+    private ShapeRenderer shapeRenderer;          //    design purposes. This can be
+//------------------------------------------------//    removed later when the design is done.
+
     public GameScreen(DoorGame doorGame) {
         this.doorGame = doorGame;
 
@@ -30,7 +36,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-
+        shapeRenderer = new ShapeRenderer(); // ADDED BY LEIBNIZ for design purposes
     }
 
     @Override
@@ -41,6 +47,8 @@ public class GameScreen implements Screen {
         // of the color to be used to clear the screen.
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        drawGrid(); // ADDED by LEIBNIZ
 
         doorGame.batch.begin();
         doorGame.font.draw(doorGame.batch, "GAME SCREEN!!", (width/2)-50, (height/2) );
@@ -67,4 +75,22 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
     }
+
+
+
+//----- METHOD ADDED BY LEIBNIZ --------------------------------------------------//
+    private void drawGrid() {                                                     //
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);                        //
+        //begin                                                                   //  This is to
+                                                                                  //  draw the grids
+        for(int x = 0; x < Gdx.graphics.getWidth(); x += GRID_CELL) {             //  on the screen
+            for(int y = 0; y < Gdx.graphics.getHeight(); y += GRID_CELL) {        //
+                shapeRenderer.rect(x,y,GRID_CELL,GRID_CELL);                      //  This can be
+            }                                                                     //  removed when
+        }                                                                         //  all designs
+                                                                                  //  are done.
+        //end                                                                     //
+        shapeRenderer.end();                                                      //
+    }                                                                             //
+//--------------------------------------------------------------------------------//
 }
